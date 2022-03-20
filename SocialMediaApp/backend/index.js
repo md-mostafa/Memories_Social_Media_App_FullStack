24786de9289e82ab13eb8c25c4e4c530a4629733
Md Mostafa Akash <md.mostafa.akash@gmail.com>
2022-03-20 11:22:31 +0600

    connecting to database

--- SocialMediaApp/backend/index.js
+++ SocialMediaApp/backend/index.js
@@ -11,3 +11,19 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.json({ limit: "30mb", extended: true}));
 
 app.use(cors());
+
+
+//connecting to database 
+const CONNECTION_URL = 'mongodb+srv://mdmostafa:mdmostafa@nodejs-authentication.4tkhl.mongodb.net/test?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+//using mongoose to connect to our database
+mongoose.connect(CONNECTION_URL, { 
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+
+//mongoose.set('useFindAndModify', false);
