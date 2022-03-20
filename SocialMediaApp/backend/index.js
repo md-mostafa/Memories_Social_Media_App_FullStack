1b2b664c3c763061443c8715a77c338e48ea9e9d
Md Mostafa Akash <md.mostafa.akash@gmail.com>
2022-03-20 11:14:26 +0600

    backend index.js setup

--- SocialMediaApp/backend/index.js
+++ SocialMediaApp/backend/index.js
@@ -3,3 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+
+
+const app = express();
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json({ limit: "30mb", extended: true}));
+
+app.use(cors());
