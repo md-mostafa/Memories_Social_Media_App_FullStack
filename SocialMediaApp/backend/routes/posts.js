39578d3fcb56c48f504a1d245494fe6d05538674
Md Mostafa Akash <md.mostafa.akash@gmail.com>
2022-03-20 16:27:47 +0600

    backend models controller folder

--- SocialMediaApp/backend/routes/posts.js
+++ SocialMediaApp/backend/routes/posts.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
+import { getPosts } from '../contollers/posts';
+
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-    res.send("Akash is the best");
-});
+router.get('/', getPosts);
 
 
 export default router;
