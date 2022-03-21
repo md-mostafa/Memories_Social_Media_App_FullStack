d27d5ae490f48073ece6a7e60fa8e058095f6178
Md Mostafa Akash <md.mostafa.akash@gmail.com>
2022-03-21 10:14:02 +0600

    delete functionality

--- SocialMediaApp/frontend/src/components/Posts/Post/Post.js
+++ SocialMediaApp/frontend/src/components/Posts/Post/Post.js
@@ -5,9 +5,14 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -32,20 +37,21 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
 
-                <CardActions className={classes.cardActions}>
-                    <Button size="small" color="primary" onClick={() =>{}}>
-                        <ThumbUpAltIcon fontSize="small" />
-                        Like
-                        {post.likeCount}
-                    </Button>
-                    <Button size="small" color="primary" onClick={() => {}}>
-                        <DeleteIcon fontSize="small" />
-                        Delete
-                    </Button>
-                </CardActions>
-                
-            </Card>
-        );
-    }
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() =>{}}>
+                    <ThumbUpAltIcon fontSize="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+                    <DeleteIcon fontSize="small" />
+                    Delete
+                </Button>
 
-    export default Post;
+            </CardActions>
+            
+        </Card>
+    );
+}
+
+export default Post;
