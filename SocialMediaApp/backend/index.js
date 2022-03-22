7c6f42625ce843109a0b13ab74bed6c75e61183c
Md Mostafa Akash <md.mostafa.akash@gmail.com>
2022-03-22 15:38:54 +0600

    backend deployment

--- SocialMediaApp/backend/index.js
+++ SocialMediaApp/backend/index.js
@@ -16,6 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Akash is the best');
+})
+
 //connecting to mongodb
 
 const PORT = process.env.PORT || 5000;
