7791c6aaf1d13763be90fd64e9274a030693cb82
Md Mostafa Akash <md.mostafa.akash@gmail.com>
2022-03-22 12:50:38 +0600

    dotenv

--- SocialMediaApp/backend/index.js
+++ SocialMediaApp/backend/index.js
@@ -1,24 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json({ limit: "30mb", extended: true}));
 app.use(cors());
 
+
 app.use('/posts', postRoutes);
 
-//connecting to database 
-const CONNECTION_URL = 'mongodb+srv://mdmostafa:mdmostafa@nodejs-authentication.4tkhl.mongodb.net/test?retryWrites=true&w=majority';
+//connecting to mongodb
+
 const PORT = process.env.PORT || 5000;
 
 //using mongoose to connect to our database
-mongoose.connect(CONNECTION_URL, { 
+mongoose.connect(process.env.CONNECTION_URL, { 
     useNewUrlParser : true,
     useUnifiedTopology: true
 })
