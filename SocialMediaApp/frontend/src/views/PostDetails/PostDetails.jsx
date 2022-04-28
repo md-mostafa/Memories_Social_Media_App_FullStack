25f77b89877ea4d35642fa105b11c97b2b0e17cf
Md Mostafa Akash <md.mostafa.akash@gmail.com>
2022-04-28 06:35:07 +0600

    recommended post

--- SocialMediaApp/frontend/src/views/PostDetails/PostDetails.jsx
+++ SocialMediaApp/frontend/src/views/PostDetails/PostDetails.jsx
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react';
 import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core/';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
-import { useParams, useNavigate  } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import { getPost, getPostsBySearch } from '../../actions/posts';
 import useStyles from './styles';
 
 const Post = () => {
-  const { post, posts, isLoading } = useSelector((state) => state.posts );
+  const { post, posts, isLoading } = useSelector((state) => state.posts);
   const dispatch = useDispatch();
-  const navigate  = useNavigate();
+  const navigate = useNavigate();
   const classes = useStyles();
   const { id } = useParams();
 
@@ -26,6 +26,7 @@ const Post = () => {
 
   if (!post) return null;
 
+  const openPost = (_id) => navigate(`/posts/${_id}`);
 
   if (isLoading) {
     return (
@@ -35,7 +36,7 @@ const Post = () => {
     );
   }
 
-
+  const recommendedPosts = posts.filter(({ _id }) => _id !== post._id);
 
   return (
     <Paper style={{ padding: '20px', borderRadius: '15px' }} elevation={6}>
@@ -56,7 +57,31 @@ const Post = () => {
           <img className={classes.media} src={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={post.title} />
         </div>
       </div>
-      
+
+      {!!recommendedPosts.length && (
+
+        <div className={classes.section}>
+
+          <Typography gutterBottom variant="h5">You might also like:</Typography>
+
+          <Divider />
+
+          <div className={classes.recommendedPosts}>
+            
+            {recommendedPosts.map(({ title, name, message, likes, selectedFile, _id }) => (
+              <div style={{ margin: '20px', cursor: 'pointer' }} onClick={() => openPost(_id)} key={_id}>
+                <Typography gutterBottom variant="h6">{title}</Typography>
+                <Typography gutterBottom variant="subtitle2">{name}</Typography>
+                <Typography gutterBottom variant="subtitle2">{message}</Typography>
+                <Typography gutterBottom variant="subtitle1">Likes: {likes.length}</Typography>
+                <img src={selectedFile} width="200px" />
+              </div>
+            ))}
+
+
+          </div>
+        </div>
+      )}
     </Paper>
   );
 };
